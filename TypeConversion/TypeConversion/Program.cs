0fd84c4cdf81c82da65658d5c67c3b49b5f15022
Nong <nong.suon@gmail.com>
2017-11-25 15:06:55 -0500

    finished lecture 19 cSharp basic

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+            
+        }
+    }
+}
