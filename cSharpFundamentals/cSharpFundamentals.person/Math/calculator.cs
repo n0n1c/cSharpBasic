9379363bfdf30333147adce5802f9cc4f027450f
Nong <nong.suon@gmail.com>
2017-11-25 15:45:09 -0500

    finished lecture 27 cSharp basic

---
+++ cSharpFundamentals/cSharpFundamentals.person/Math/calculator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace cSharpFundamentals.Math
+{
+    public class calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }   
+    }
+}
---
+++ cSharpFundamentals/cSharpFundamentals.person/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cSharpFundamentals.Math;
+
+namespace cSharpFundamentals.person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.firstName = "John";
+            john.lastName = "Smith";
+            john.Introduce();
+
+            calculator calculator = new calculator();
+            var result = calculator.add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
