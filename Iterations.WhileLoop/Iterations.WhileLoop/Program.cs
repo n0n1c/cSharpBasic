b6131bf262c894eac5327fbf6eb2cd71d1e6c9b6
Nong <nong.suon@gmail.com>
2017-11-29 11:05:09 -0500

    finished lecture 46 Udemy CSharp basic

---
+++ Iterations.WhileLoop/Iterations.WhileLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations.WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+
+            //    i++;
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+
+        }
+    }
+}
