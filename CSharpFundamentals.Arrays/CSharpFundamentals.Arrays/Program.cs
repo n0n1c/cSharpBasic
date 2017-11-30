4985fe69b4c410457438c42d7b95a456472dc43b
Nong <nong.suon@gmail.com>
2017-11-30 18:01:41 -0500

    finished lecture 52 Udemy CSharp basic

---
+++ CSharpFundamentals.Arrays/CSharpFundamentals.Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers= new[] {3, 7, 9, 2, 14, 6};
+
+            //length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            //Index of
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            //clear
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //copy
+            var another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+            
+            //sort
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
