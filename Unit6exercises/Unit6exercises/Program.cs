a10e6c22d8d7748d8414d76c0d03b1789e145694
Nong <nong.suon@gmail.com>
2017-12-04 20:18:29 -0500

    finished unit 6 exercise 5

--- Unit6exercises/Unit6exercises/Program.cs
+++ Unit6exercises/Unit6exercises/Program.cs
@@ -47,7 +47,7 @@ namespace Unit6exercises
 
             while (array3.Count < 5)
             {
-                Console.WriteLine("Enter 5 number: ");
+                Console.WriteLine("Enter 5 numbers: ");
                 var input3 = Convert.ToInt32(Console.ReadLine());
 
                 if (array3.Contains(input3))
@@ -92,6 +92,42 @@ namespace Unit6exercises
                 Console.WriteLine(number);
             }
 
+            //exercise 5
+            string[] elements;
+            while (true)
+            {
+                Console.Write("Enter 5 or more comma separated numbers: ");
+                var input5 = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input5))
+                {
+                    elements = input5.Split(',');
+                    if (elements.Length >= 5)
+                        break;
+                }
+                Console.WriteLine("Invalid list, please re-try.");
+            }
+
+            var numbers5 = new List<int>();
+            foreach (var number in elements)
+            {
+                numbers5.Add(Convert.ToInt32(number));
+            }
+
+            var smallestNums = new List<int>();
+            while (smallestNums.Count < 3)
+            {
+                var min = numbers5[0];
+                foreach (var number in numbers5)
+                {
+                    if (number < min)
+                        min = number;
+                }
+                smallestNums.Add(min);
+
+                numbers5.Remove(min);
+            }
+            Console.WriteLine("The three smallest numbers: {0}, {1}, {2}", smallestNums[0], smallestNums[1], smallestNums[2]);
         }
     }
 }
