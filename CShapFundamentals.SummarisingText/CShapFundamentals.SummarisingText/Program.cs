30c580f3804c8b70481c6dd7c4e7209e1520616e
Nong <nong.suon@gmail.com>
2017-12-05 19:23:02 -0500

    finished lecture 64 Udemy CSharp basic

---
+++ CShapFundamentals.SummarisingText/CShapFundamentals.SummarisingText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CShapFundamentals.SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really, really, really, really, really, long text.";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+    }
+}
