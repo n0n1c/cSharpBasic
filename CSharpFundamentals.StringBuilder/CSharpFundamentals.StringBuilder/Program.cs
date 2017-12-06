1f6a45a5808d83d72c534af725a1500f533e4064
Nong <nong.suon@gmail.com>
2017-12-05 19:54:43 -0500

    finished lecture 66 Udemy CSharp basic

---
+++ CSharpFundamentals.StringBuilder/CSharpFundamentals.StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("Hello World!");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+            Console.WriteLine("First char: " + builder[0]);
+        }
+    }
+}
