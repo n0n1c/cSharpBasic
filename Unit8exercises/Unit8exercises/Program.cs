35c79260401045f9da6238bcff22c0335e24adaf
Nong <nong.suon@gmail.com>
2017-12-05 20:31:37 -0500

    Finished unit 8 exercise 1

---
+++ Unit8exercises/Unit8exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit8exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few numbers separated by hyphens: ");
+            var input1 = Console.ReadLine();
+
+            var numbers1 = new List<int>();
+            foreach (var number in input1.Split('-'))
+            {
+                numbers1.Add(Convert.ToInt32(number));
+            }
+
+            numbers1.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers1.Count; i++)
+            {
+                if (numbers1[i] != numbers1[i - 1] + 1)
+                {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+
+            var message = isConsecutive ? "Consecuive" : "Not Consecutive";
+            Console.WriteLine(message);
+        }
+    }
+}
