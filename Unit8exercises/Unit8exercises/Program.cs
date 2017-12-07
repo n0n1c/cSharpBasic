66c51b7988700fb97e2fe810ec5c54848a7ad248
Nong <nong.suon@gmail.com>
2017-12-07 15:23:58 -0500

    finished unit 8 exercise 2

--- Unit8exercises/Unit8exercises/Program.cs
+++ Unit8exercises/Unit8exercises/Program.cs
@@ -33,6 +33,35 @@ namespace Unit8exercises
 
             var message = isConsecutive ? "Consecuive" : "Not Consecutive";
             Console.WriteLine(message);
+
+            //Exercise 2
+
+            Console.Write("Enter a few hyphen separated numbers: ");
+            var input2 = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input2))
+                return;
+
+            var numbers2 = new List<int>();
+
+            foreach (var number in input2.Split('-'))
+                numbers2.Add(Convert.ToInt32(number));
+            
+            var uniqueNums = new List<int>();
+            var isDuplicate = false;
+            foreach (var number in numbers2)
+            {
+                if (!uniqueNums.Contains(number))
+                    uniqueNums.Add(number);
+                else
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (isDuplicate)
+                Console.WriteLine("Duplicate");
         }
     }
 }
