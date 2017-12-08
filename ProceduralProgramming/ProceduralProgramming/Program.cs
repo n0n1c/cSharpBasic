2b1d07f0770d7c8bccd83454c5389dccaa570125
Nong <nong.suon@gmail.com>
2017-12-07 19:54:06 -0500

    finished lecture 68 example 1

---
+++ ProceduralProgramming/ProceduralProgramming/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Whats your name?: ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+            
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+            return new string(array);
+        }
+    }
+}
