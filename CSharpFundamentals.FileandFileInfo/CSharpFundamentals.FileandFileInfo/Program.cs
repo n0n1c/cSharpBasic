1379b310f90e79b8cde1babf944bb29476f79349
Nong <nong.suon@gmail.com>
2017-12-16 11:07:15 -0500

    finished lecture 72 CSharp basic

--- CSharpFundamentals.FileandFileInfo/CSharpFundamentals.FileandFileInfo/Program.cs
+++ CSharpFundamentals.FileandFileInfo/CSharpFundamentals.FileandFileInfo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace CSharpFundamentals.FileandFileInfo
     {
         static void Main(string[] args)
         {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
         }
     }
 }
