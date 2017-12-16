d07f8cf9f76d6f2e326534e08eeda9982c31a5ac
Nong <nong.suon@gmail.com>
2017-12-16 11:26:11 -0500

    finished lecture 73 CSharp basic

---
+++ CSharpFundamentals.DirectoryandDirectoryInfo/CSharpFundamentals.DirectoryandDirectoryInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.DirectoryandDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //var files = Directory.GetFiles(@"c:\users\nonic\source", "*.sln", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file);
+            //}
+
+            var directories = Directory.GetDirectories(@"c:\users\nonic\source\repos", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+        }
+    }
+}
