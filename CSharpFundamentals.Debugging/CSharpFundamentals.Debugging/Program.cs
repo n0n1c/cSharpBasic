ddcfecaa8a7408bd2381a65f68c6d4a712867127
Nong <nong.suon@gmail.com>
2017-12-18 20:34:12 -0500

    finished lecture 80 Udemy CSharp basic

--- CSharpFundamentals.Debugging/CSharpFundamentals.Debugging/Program.cs
+++ CSharpFundamentals.Debugging/CSharpFundamentals.Debugging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,12 @@ namespace CSharpFundamentals.Debugging
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if (list ==null)
+                throw new ArgumentNullException("list");
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list.");
+
             var buffer = new List<int>(list);
             var smallests = new List<int>();
 
