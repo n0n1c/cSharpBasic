4f57593f0c1a055b179a6a91e1e0ac1bccdf2665
Nong <nong.suon@gmail.com>
2018-01-20 23:18:46 -0500

    Implemented TestCases

--- UnitTesting.GettingStarted.Tests/UnitTesting.GettingStarted.Tests/CalculatorTests.cs
+++ UnitTesting.GettingStarted.Tests/UnitTesting.GettingStarted.Tests/CalculatorTests.cs
@@ -10,11 +10,15 @@ namespace UnitTesting.GettingStarted.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        [Test]
-        public void Add_Always_ReturnsExpectedResults()
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 5)]
+        [TestCase(1000, 1)]
+        public void Add_Always_ReturnsExpectedResults(int lhs, int rhs)
         {
             var systemUnderTest = new Calculator();
-            Assert.That(systemUnderTest.Add(1, 2), Is.EqualTo(3));
+            var expected = lhs + rhs;
+            Assert.That(systemUnderTest.Add(lhs, rhs), Is.EqualTo(expected));
         }
     }
 }
